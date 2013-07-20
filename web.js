652709756c569ef96d4f6117bb4e3b632e1f014a
Ubuntu <ubuntu@ip-10-139-0-97.ec2.internal>
2013-07-20 13:02:28 +0000

    separated css, modified web.js to do so

--- web.js
+++ web.js
@@ -4,10 +4,19 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var b = fs.readFileSync('index.html');
+    //var b = fs.readFileSync('index.html');
     
-    response.send(b.toString());
+    //response.send(b.toString());
+    response.sendfile('index.html', {root: __dirname});
 });
+
+app.get('/css/style.css', function(request, response) {
+    var b = fs.readFileSync('css/style.css');
+    //response.setHeader('Content-Type', 'stylesheet');
+    console.log(__dirname + request.params[0]);
+    response.sendfile('css/style.css', {root: __dirname});
+});
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
